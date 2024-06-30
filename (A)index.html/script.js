1067c86056b6cb5c872d0c8c273c36e611dddf5c
Vamshi17c <157137156+Vamshi17c@users.noreply.github.com>
2024-07-01 01:29:01 +0530

    Add files via upload

--- (A)index.html/script.js
+++ (A)index.html/script.js
@@ -1,152 +1,70 @@
-window.onload = function(){
-    const day = document.getElementById("day");
-    const month = document.getElementById("month");
-    const year = document.getElementById("year");
-    const labels = document.getElementsByTagName("label");
-    const error = document.getElementsByClassName("error");
-    const submitButton = document.getElementById("submit");
-    const spans = document.getElementsByTagName("span");
+// Define funtion to get calculated Age
+function getDOB() {
 
-    const date = new Date();
+    // Getting input from html input element
+    let data =
+        document.getElementById("inputDob").value;
 
-    let currentDay = date.getDate();
-    let currentMonth = date.getMonth() + 1;
-    let currentYear = date.getFullYear();
+    // Convert input data to usable format
+    // as day,month and year
+    let dob = new Date(data);
+    let day = dob.getDate();
+    let month = dob.getMonth();
+    let year = dob.getFullYear();
 
-    const typeOfError = [
-        "",
-        "This field is required",
-        "Must be a valid day",
-        "Must be a valid month",
-        "Must be a valid year",
-        "Must be a valid date"
-    ];
+    // Getting current date and calculating the difference
+    let now =
+        new Date(document.getElementById("cdate").value);
+    console.log(now);
+    let yearDiff = now.getFullYear() - year;
+    let monthDiff = now.getMonth() - month;
+    let dateDiff = now.getDate() - day;
 
-    const errorState = (numberOfError, typeOfDate, typeOfError, color) => {
-        error[numberOfError].innerHTML = typeOfError;
-        labels[numberOfError].style.color = color;
-        typeOfDate.style.borderColor = color;
+    // Calculating the Age
+    if (yearDiff < 0) console.log("invalid date");
+    else if (monthDiff > 0) {
+        console.log(yearDiff);
+    } else if (monthDiff === 0 && dateDiff >= 0) {
+        console.log(yearDiff);
+    } else {
+        yearDiff = yearDiff - 1;
+        if (monthDiff <= 0)
+            if (dateDiff > 0) monthDiff = 12 + monthDiff;
+            else monthDiff = 11 - monthDiff;
+    }
+    if (dateDiff < 0) {
+        dateDiff = 30 + dateDiff;
+        monthDiff -= 1;
     }
 
-    const isLeapYear = (day, month, year) => {
-        month = month - 1;
-        fullDate = new Date(year,month,day);
-        if (day == fullDate.getDate() && month == fullDate.getMonth() && year == fullDate.getFullYear())
-          return true;
-        else
-          return false
-    }
-
-    const substractAge = () => {
-        let newYear = Math.abs(currentYear - year.value);
-
-        let newMonth = 0;
-        if(currentMonth >= month.value){
-            newMonth = currentMonth - month.value;
-        }
-        else{
-            newYear--;
-            newMonth = 12 + currentMonth - month.value;
-        }
-
-        let newDay = 0;
-        if(currentDay >= day.value){
-            newDay = currentDay - day.value;
-        }
-        else{
-            newMonth--;
-            if(isLeapYear(day.value, month.value, year.value)){
-                newDay = 30 + currentDay - day.value;
-            }
-            else{
-                newDay = currentDay - day.value;
-            }
-
-            if(newMonth < 0){
-                newMonth = 11;
-                newYear--;
-            }
-            if(newMonth < currentMonth){
-                newDay++;
-            }
-        }
-
-        spans[0].innerHTML = newYear;
-        spans[1].innerHTML = newMonth;
-        spans[2].innerHTML = newDay;
-    }
-
-    const isDayCorrect = () => {
-        if(day.value == ""){
-            errorState(0, day, typeOfError[1], "#ff5757");
-            return false;
-        }
-        else if(day.value <= 0 || day.value > 31){
-            errorState(0, day, typeOfError[2], "#ff5757");
-            return false;
-        }
-        else if(isLeapYear(day.value, month.value, year.value) == false){
-            errorState(0, day, typeOfError[5] , "#ff5757");
-            return false;
-        }
-        else{
-            errorState(0, day, typeOfError[0], "");
-            return true;
-        }
-    }
-
-    const isMonthCorrect = () => {
-        if(month.value == ""){
-            errorState(1, month, typeOfError[1], "#ff5757");
-            return false;
-        }
-        else if(month.value <= 0 || month.value > 12){
-            errorState(1, month, typeOfError[3], "#ff5757");
-            return false;
-        }
-        else if(isLeapYear(day.value, month.value, year.value) == false){
-            errorState(1, month, typeOfError[0], "#ff5757");
-            return false;
-        }  
-        else{
-            errorState(1, month, typeOfError[0], "");
-            return true;
-        }
-    }
-
-    const isYearCorrect = () => {
-        if(year.value == ""){
-            errorState(2, year, typeOfError[1], "#ff5757");
-            return false;
-        }
-        else if(year.value > currentYear){
-            errorState(2, year, typeOfError[4], "#ff5757");
-            return false;
-        }
-        else if(year.value == currentYear && month.value > currentMonth){
-            errorState(1, month, typeOfError[3], "#ff5757");
-            return false;
-        }
-        else if(year.value == currentYear && month.value == currentMonth && day.value > currentDay){
-            errorState(0, day, typeOfError[2], "#ff5757");
-            return false;
-        }
-        else if(isLeapYear(day.value, month.value, year.value) == false){
-            errorState(2, year, typeOfError[0], "#ff5757");
-            return false;
-        }   
-        else{
-            errorState(2, year, typeOfError[0], "");
-            return true;
-        }
-    }
-
-    submitButton.addEventListener("click", () => {
-        isDayCorrect();
-        isMonthCorrect();
-        isYearCorrect();
-        if(isDayCorrect() && isMonthCorrect() && isYearCorrect()){
-            substractAge();
-        }
-    })
+    // Show calculated age as output
+    // and invalid if wrong input is given
+    if (yearDiff < 0)
+        document.getElementById("currentAge").innerHTML = "Invalid Date";
+    else
+        document.getElementById("currentAge").innerHTML =
+            "Your current Age is " + yearDiff + " years "
+            + monthDiff + " months " + dateDiff + " days";
 }
+
+// Function to provide default date value
+function currentDate() {
+    console.log(formatted());
+    let d = document.getElementById("cdate");
+    d.value = formatted();
+}
+
+function formatted(date = new Date()) {
+    return [
+        date.getFullYear(),
+        short(date.getMonth() + 1),
+        short(date.getDate()),
+    ].join("-");
+}
+function short(num) {
+    return num.toString().padStart(2, "0");
+}
+
+// Calling current date function
+// to set default date value
+currentDate();
